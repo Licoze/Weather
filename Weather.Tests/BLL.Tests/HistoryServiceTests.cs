9c0682d7b6ddca4e542fed97eb4bf95aa08c02d0
Licoze <vlad-melnik-96@yandex.ru>
2017-07-16 09:46:04 +0300

    Controller Tests

---
+++ Weather.Tests/BLL.Tests/HistoryServiceTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Castle.Components.DictionaryAdapter;
+using NUnit.Framework;
+using Weather.BLL.DTO;
+using Weather.BLL.Services;
+using Weather.DAL.Interfaces;
+using Weather.DAL.Models;
+using Weather.Models;
+using Weather.Tests.Config;
+using Weather.Tests.Mock;
+
+namespace Weather.Tests.BLL.Tests
+{
+    [TestFixture]
+    class HistoryServiceTests
+    {
+        private HistoryService service;
+        private IMapper _mapper;
+        private IUnitOfWork _uow;
+        private List<City> _cities;
+        private List<Forecast> _forecasts;
+        private List<SearchHistory> _histories;
+        private List<WeatherUnit> _units;
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MapperProfile>();
+            });
+            _mapper = config.CreateMapper();
+            _uow=new MockUoW();
+            _uow.Histories.Create(new SearchHistory()
+            {
+                Id = 31
+            });
+            service = new HistoryService(_uow, _mapper);
+        }
+
+        [Test]
+        public void SaveChangesTest()
+        {
+            var forecastDto = new ForecastDTO();
+            forecastDto.Id = 1;
+            var result=service.SaveToHistory(forecastDto).Result;
+            Assert.AreEqual(0,result);
+        }
+        [Test]
+        public void GetHistoryTest()
+        {
+
+            var history = _uow.Histories.GetAll().FirstOrDefault(x => x.Id == 31);
+            var expected = _mapper.Map<SearchHistoryDTO>(history);
+            var result = service.GetHistory();
+            Assert.AreEqual(result.FirstOrDefault().Id,expected.Id);
+        }
+    }
+}
---
+++ Weather.Tests/Mock/MockUoW.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Weather.DAL.Infrastructure;
+using Weather.DAL.Interfaces;
+using Weather.DAL.Models;
+using Weather.DAL.Repositories;
+
+namespace Weather.Tests.Mock
+{
+    public class MockUoW:IUnitOfWork
+    {
+        private MockRepository<City> _cities;
+        private MockRepository<Forecast> _forecasts;
+        private MockRepository<SearchHistory> _histories;
+        private MockRepository<WeatherUnit> _weatherUnits;
+        private bool _disposed;
+
+        public IRepository<City> Cities 
+        {
+            get
+            {
+                _cities = _cities ?? new MockRepository<City>();
+                return _cities;
+            }
+        }
+
+        public IRepository<Forecast> Forecasts
+        {
+            get
+            {
+                _forecasts = _forecasts ?? new MockRepository<Forecast>();
+                return _forecasts;
+            }
+        }
+
+        public IRepository<SearchHistory> Histories
+        {
+            get
+            {
+                _histories = _histories ?? new MockRepository<SearchHistory>();
+                return _histories;
+            }
+        }
+
+        public IRepository<WeatherUnit> WeatherUnits
+        {
+            get
+            {
+                _weatherUnits = _weatherUnits ?? new MockRepository<WeatherUnit>();
+                return _weatherUnits;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            
+        }
+
+        public int Save()
+        {
+            return 0;
+        }
+        public async Task<int> SaveAsync()
+        {
+            return 0;
+        }
+    }
+}
