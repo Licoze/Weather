e6aa45661c6dfc86baa5bf15a3de41787f25081c
Licoze <vlad-melnik-96@yandex.ru>
2017-07-18 23:27:29 +0300

    Api Controllers

---
+++ Weather/Controllers/WebApi/HistoryController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AutoMapper;
+using Weather.BLL.Infrastructure;
+using Weather.BLL.Interfaces;
+using Weather.Models;
+
+namespace Weather.Controllers.WebApi
+{
+    public class HistoryController : ApiController
+    {
+        private IHistoryService _service;
+        private IMapper _mapper;
+
+        public HistoryController(IServiceFactory factory, IMapper mapper)
+        {
+            _service = factory.CreateHistoryService();
+            _mapper = mapper;
+        }
+        // GET: api/History
+        public IHttpActionResult Get()
+        {
+            var resultDto = _service.GetHistory();
+            var result = _mapper.Map<List<SearchHistoryViewModel>>(resultDto);
+            if (result != null)
+                return Json(result);
+            return NotFound();
+        }
+
+        // GET: api/History/5
+        public IHttpActionResult Get(int id)
+        {
+            var resultDto = _service.GetHistory().FirstOrDefault(x=>x.Id==id);
+            var result = _mapper.Map<SearchHistoryViewModel>(resultDto);
+            if (result != null)
+                return Json(result);
+            return NotFound();
+        }
+
+    }
+}
---
+++ Weather/Controllers/WebApi/WeatherController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AutoMapper;
+using Weather.BLL.Infrastructure;
+using Weather.BLL.Interfaces;
+using Weather.Models;
+
+namespace Weather.Controllers.WebApi
+{
+    public class WeatherController : ApiController
+    {
+        private IWeatherService _service;
+        private IMapper _mapper;
+        public WeatherController(IServiceFactory factory, IMapper mapper)
+        {
+            _service = factory.CreateWeatherService();
+            _mapper = mapper;
+        }
+        // GET: api/Weather
+        [Route("api/Weather/{city}")]
+        public IHttpActionResult Get(string city)
+        {
+            var resultDto = _service.GetWeatherDaily(city);
+            var result = _mapper.Map<ForecastViewModel>(resultDto);
+            if (result != null)
+                return Json(result);
+            return NotFound();
+        }
+
+        // GET: api/Weather/Dnipro/5
+        [Route("api/Weather/{city}/{count}")]
+        public IHttpActionResult Get(string city,int count)
+        {
+            var resultDto = _service.GetWeatherDaily(city,count);
+            var result = _mapper.Map<ForecastViewModel>(resultDto);
+            if (result != null)
+                return Json(result);
+            return NotFound();
+        }
+
+
+    }
+}
